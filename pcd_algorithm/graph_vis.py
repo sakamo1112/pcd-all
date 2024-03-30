9348d20f9e3241a7dbf7806eed02f5a23f0889ae
坂本海人 <sakamo78911@gmail.com>
2024-03-30 20:09:52 +0900

    カンマ区切りに対応

--- pcd_algorithm/graph_vis.py
+++ pcd_algorithm/graph_vis.py
@@ -3,14 +3,7 @@ import argparse
 import matplotlib.pyplot as plt
 import networkx as nx  # type: ignore
 import pandas as pd  # type: ignore
-import pandera as pa  # type: ignore
 import polars as pl
-
-
-class Schema(pa.DataFrameModel):
-    edge_number: int = pa.Field(ge=0)
-    node_start: int = pa.Field(ge=0)
-    node_end: int = pa.Field(ge=0)
 
 
 def draw_graph(G: nx.Graph) -> None:
@@ -34,7 +27,6 @@ def draw_graph(G: nx.Graph) -> None:
         pos = layout(G)
         centrality = nx.degree_centrality(G)  # 次数中心性の計算
         node_color = [centrality[node] for node in G.nodes]
-        print(node_color)
         nx.draw(
             G,
             pos,
@@ -60,6 +52,9 @@ def draw_graph_3d(G: nx.Graph) -> None:
     Parameters:
     G (nx.Graph): 描画するグラフ
     """
+    gate_nodes = [1, 2, 3, 4, 102]  # ゲートノードの番号をリストで指定
+    ver_flow_nodes = [6, 7, 8, 9, 10]  # 特定のノード番号をリストで指定
+
     # 3Dのspring layoutを計算
     pos = nx.kamada_kawai_layout(G, dim=3)
 
@@ -67,18 +62,30 @@ def draw_graph_3d(G: nx.Graph) -> None:
     fig = plt.figure(figsize=(10, 7))
     ax = fig.add_subplot(111, projection='3d')
 
-    centrality = nx.degree_centrality(G)  # 次数中心性の計算
-    node_color = [centrality[node] for node in G.nodes]  # 色の強度を調整
+    centrality = nx.betweenness_centrality(G)  # 中心性の計算
 
-    for key, value in pos.items():
-        ax.text(value[0], value[1], value[2], f'{key}', color='black', fontsize=3)
+    for node, value in pos.items():
+        ax.text(value[0], value[1], value[2], f'{node}', color='black', fontsize=3)
 
     xs, ys, zs = [], [], []
-    for key, value in pos.items():
+    node_color = []
+    gate_flows = []
+    vertical_flows = []
+    for node, value in pos.items():
         xs.append(value[0])
         ys.append(value[1])
         zs.append(value[2])
+        node_color.append(centrality[node])  # 通常のノードの色
+        if node in gate_nodes:
+            gate_flows.append([value[0], value[1], value[2]])
+        elif node in ver_flow_nodes:  # 特定のノードの場合
+            vertical_flows.append([value[0], value[1], value[2]])
     ax.scatter(xs, ys, zs, c=node_color, cmap='viridis', depthshade=True)
+
+    for i in range(len(gate_nodes)):
+        ax.scatter(gate_flows[i][0], gate_flows[i][1], gate_flows[i][2], c='green', s=100)
+    for j in range(len(ver_flow_nodes)):
+        ax.scatter(vertical_flows[j][0], vertical_flows[j][1], vertical_flows[j][2], c='violet', s=100)
 
     # エッジを描画
     for edge in G.edges():
@@ -92,7 +99,8 @@ def draw_graph_3d(G: nx.Graph) -> None:
 
 def main(xlsx_path: str, sheet_name: str) -> pd.DataFrame:
     """
-    指定されたsheetのデータを抽出し、DataFrameを作成します。
+    指定されたsheetのデータを抽出し、DataFrameを作成する。
+    node_endがカンマで区切られている場合、それぞれのnode_endに対してedgeを作成する。
 
     Parameters:
     data (pd.DataFrame): 全データが格納されているDataFrame
@@ -102,27 +110,31 @@ def main(xlsx_path: str, sheet_name: str) -> pd.DataFrame:
     pd.DataFrame: 抽出されたデータが格納されたDataFrame
     """
     data = pl.read_excel(xlsx_path, sheet_name=sheet_name).to_pandas()
-    #data = Schema.validate(data)
 
     G = nx.Graph()
     for i in range(len(data.index)):
-        edge_name = (
-            "Edge"
-            + str(i)
-            + " ("
-            + str(data.iloc[i, 1])
-            + "-"
-            + str(data.iloc[i, 2])
-            + ")"
-        )
-        shop_vector = list(data.iloc[i, 3:])
-        G.add_edge(
-            data.iloc[i, 1],
-            data.iloc[i, 2],
-            label="",
-            name=edge_name,
-            vector_attribute=shop_vector,
-        )
+        if isinstance(data.iloc[i, 2], str):
+            end_node_list = data.iloc[i, 2].split(',')
+        else:
+            end_node_list = [data.iloc[i, 2]]
+        for end_node in end_node_list:
+            edge_name = (
+                "Edge "
+                + "("
+                + str(data.iloc[i, 1])
+                + "→"
+                + str(end_node)
+                + ")"
+            )
+            print(edge_name)
+            shop_vector = list(data.iloc[i, 3:])
+            G.add_edge(
+                data.iloc[i, 1],
+                int(end_node),
+                label="",
+                name=edge_name,
+                vector_attribute=shop_vector,
+            )
 
     draw_graph(G)
     draw_graph_3d(G)
